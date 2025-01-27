eb5cb41105385d4cc5b2c91e5255e0c83364467e
Niuguin <105677816+regymatrix@users.noreply.github.com>
2025-01-27 10:58:44 -0300

    Script com produto

--- BancoDeDados/01_ScriptCriadoBD.sql
+++ BancoDeDados/01_ScriptCriadoBD.sql
@@ -10,4 +10,16 @@ CREATE TABLE CATEGORIA (
   Codigo int identity(1,1) primary key,
   Descricao varchar(100) not null
 )
+GO
+CREATE TABLE PRODUTO(
+  Codigo int identity(1,1) primary key,
+  Descricao varchar(100) not null,
+  UnidadeMedida varchar(100) not null,
+  EstoqueAtual FLOAT(2) DEFAULT '0.00',
+  Epermanente bit DEFAULT 0,
+  CodigoCategoria int not null
+)
+go
 
+ALTER TABLE PRODUTO
+ADD FOREIGN KEY  (CodigoCategoria) REFERENCES CATEGORIA(Codigo);
